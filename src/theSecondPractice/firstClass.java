06ad9197152e7868c5035ee20a53fed05a8a4a78
rmautia <raphaelnyangenya@gmail.com>
2022-06-10 05:25:28 +0300

    our first class

---
+++ src/theSecondPractice/firstClass.java
@@ -0,0 +1,11 @@
+package theSecondPractice;
+
+public class firstClass {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello everyone welcome to day two practice.");
+        System.out.println("Hello");
+    }
+}
